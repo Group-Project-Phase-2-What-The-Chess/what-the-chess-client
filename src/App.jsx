5df54e41bbfae65676864fdb6222067f1d5e671e
wahyudiaditya <wahyudiaditya007@gmail.com>
2025-01-07 18:13:38 +0700

    - Add chess board - Install package socket.io-client, and react-chessboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import Homepage from "./pages/Homepage";
+import InGamePage from "./pages/InGamePage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/chess" element={<InGamePage />} />
       </Routes>
     </BrowserRouter>
   );
